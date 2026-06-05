064ed9f050bad549fa6213e8db4a284ce97b83c0
lily <lily@mindnodeair.com>
2026-06-05 11:09:01 +0800

    style: 更新卡片RWD

--- app/components/card/ProjectCard.vue
+++ app/components/card/ProjectCard.vue
@@ -1,17 +1,28 @@
 <template>
-  <div class="container">
-    <div
-      v-for="ans in 3"
-      :key="ans"
-      ref="tiltRef"
-      class="tilt-card"
-      data-tilt
-      data-tilt-reverse="true"
-    >
-      <div class="title">title</div>
-      <div class="desc">desc</div>
+  <section>
+    <!-- pc版 -->
+    <div class="container">
+      <div
+        v-for="ans in 3"
+        :key="ans"
+        ref="tiltRef"
+        class="tilt-card"
+        data-tilt
+        data-tilt-reverse="true"
+      >
+        <div class="title">title</div>
+        <div class="desc">desc</div>
+      </div>
     </div>
-  </div>
+
+    <!-- 手機版 -->
+    <div class="mobile-item">
+      <div v-for="ans in 3" :key="ans" class="item-content">
+        <div class="title">title</div>
+        <div class="desc">desc</div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
@@ -47,11 +58,14 @@ onBeforeUnmount(() => {
 
 <style scoped lang="scss">
 .container {
-  display: flex;
-  justify-content: center;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
   align-items: center;
   min-height: 100vh;
   gap: 1rem;
+  @include media(1200) {
+    display: none;
+  }
 }
 
 .tilt-card {
@@ -83,5 +97,38 @@ onBeforeUnmount(() => {
   margin-top: 16px;
 
   transform: translateZ(80px);
+}
+
+//手機
+.mobile-item {
+  display: none;
+  @include media(1200) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    padding: 5rem 1rem;
+    gap: 1rem;
+    .item-content {
+      @include flex-center(column, 1rem);
+      min-height: 400px;
+      border: 4px solid var(--background-gary);
+      border-radius: 8px;
+      border-bottom-right-radius: 50%;
+      &:nth-child(2) {
+        grid-row: span 2;
+        border-top-left-radius: 60%;
+      }
+      &:nth-child(3) {
+        border-top-left-radius: 50%;
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+      }
+    }
+  }
+  @include media(768) {
+    grid-template-columns: 1fr;
+    .item-content {
+      border: none;
+    }
+  }
 }
 </style>
